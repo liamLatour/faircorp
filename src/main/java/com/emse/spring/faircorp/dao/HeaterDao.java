d5e9b4b1a42139d3b832dbeeb4a59eb0ab7e48bd
liam latour <liam.latour@gmail.com>
2022-09-28 14:59:27 +0200

    Added a method to remove heaters by room id

--- src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
@@ -2,8 +2,14 @@ package com.emse.spring.faircorp.dao;
 
 import com.emse.spring.faircorp.model.Heater;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface HeaterDao extends JpaRepository<Heater, Long> {
     Heater findById(@Param("id") String name);
+
+    @Modifying
+    @Query("delete from Heater c where c.room.id=:id")
+    void deleteByRoom(@Param("id") Long id);
 }
