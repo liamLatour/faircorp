740606ac23c4711906845c13d98a08f4cf1dc510
liam latour <liam.latour@gmail.com>
2022-09-28 14:51:33 +0200

    Implemented methods to remove windows by room id and find open windows in room

--- src/main/java/com/emse/spring/faircorp/dao/WindowDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/WindowDao.java
@@ -2,8 +2,17 @@ package com.emse.spring.faircorp.dao;
 
 import com.emse.spring.faircorp.model.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface WindowDao extends JpaRepository<Window, Long> {
+public interface WindowDao extends JpaRepository<Window, Long>, WindowDaoCustom {
     Window findById(@Param("id") String name);
+
+    @Query("select c from Window c where c.name=:name")
+    Window findByName(@Param("name") String name);
+
+    @Modifying
+    @Query("delete from Window c where c.room.id=:id")
+    void deleteByRoom(@Param("id") Long id);
 }
